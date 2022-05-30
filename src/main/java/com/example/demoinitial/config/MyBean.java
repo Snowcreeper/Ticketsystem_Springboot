8f86b53c908d8fd7f44531966b2054949995acf0
bacn <mbachmann@trdi.io>
2022-05-30 20:10:49 +0200

    Beginn of MyBean

--- src/main/java/com/example/demoinitial/config/MyBean.java
+++ src/main/java/com/example/demoinitial/config/MyBean.java
@@ -1,13 +1,22 @@
 package com.example.demoinitial.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyBean implements CommandLineRunner {
+
+    MyComponent myComponent;
+    @Autowired
+    public MyBean(MyComponent myComponent) {
+        this.myComponent = myComponent;
+    }
     @Override
     public void run(String... args) throws Exception {
+
         System.out.println("Command Line Runner ");
+        myComponent.hello();
 
     }
 }
---
+++ src/main/java/com/example/demoinitial/config/MyComponent.java
@@ -0,0 +1,10 @@
+package com.example.demoinitial.config;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyComponent {
+    public void hello() {
+        System.out.println("Ich bin MyComponent");
+    }
+}
