edd7adc6ca03857beccc26a71546816d8c6dc767
bacn <mbachmann@trdi.io>
2022-06-06 18:38:30 +0200

    LE04

--- src/main/java/com/example/demoinitial/DemoInitialApplication.java
+++ src/main/java/com/example/demoinitial/DemoInitialApplication.java
@@ -1,15 +1,31 @@
 package com.example.demoinitial;
 
+import com.example.demoinitial.utils.HasLogger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@SpringBootApplication
-public class DemoInitialApplication {
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
 
+@SpringBootApplication
+public class DemoInitialApplication implements HasLogger {
+
+    @Autowired
+    private Environment env;
     public static void main(String[] args) {
         SpringApplication.run(DemoInitialApplication.class, args);
     }
+
+    @PostConstruct
+    public void afterInit() {
+        boolean hasDevProfile = Arrays.asList(env.getActiveProfiles()).contains("dev");
+        getLogger().info("Active Profiles: " + Arrays.toString(env.getActiveProfiles()) + "\n\n");
+    }
+
+
 }
