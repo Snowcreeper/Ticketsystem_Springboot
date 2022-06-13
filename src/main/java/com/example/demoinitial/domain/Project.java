30cc4434b9ddb8959ab8897d3b5364390db373e8
bacn <mbachmann@trdi.io>
2022-06-13 17:49:07 +0200

    LE06 mit Task 6

---
+++ src/main/java/com/example/demoinitial/domain/Project.java
@@ -0,0 +1,50 @@
+package com.example.demoinitial.domain;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorValue(value = "Project")
+@DiscriminatorColumn(name = "type", discriminatorType = DiscriminatorType.STRING)
+public abstract class Project extends BaseEntity {
+
+    private String name;
+
+    @Column(name = "type", insertable = false, updatable = false)
+    protected String type;
+
+    @ManyToMany
+    private Set<Employee> employees = new HashSet<Employee>();
+
+    public Project(){}
+
+    public Project(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(Set<Employee> employees) {
+        this.employees = employees;
+    }
+}
